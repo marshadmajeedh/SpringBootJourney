e261a8d0345aaa1b55957d17e2f96f81795de5d4
Marshad <marshadahamedh@gmail.com>
2026-08-06 11:51:51 +0530

    feat: web end point for crud operations

---
+++ Simple Notes API/src/main/java/com/bank/simplenotesapi/controller/NoteController.java
@@ -0,0 +1,44 @@
+package com.bank.simplenotesapi.controller;
+
+import com.bank.simplenotesapi.dto.request.NoteRequest;
+import com.bank.simplenotesapi.dto.response.NoteResponse;
+import com.bank.simplenotesapi.service.NoteService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class NoteController {
+    private final NoteService noteService;
+
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @PostMapping("/notes")
+    public ResponseEntity<NoteResponse> createANote(@RequestBody NoteRequest noteRequest){
+        NoteResponse noteResponse = noteService.createNote(noteRequest);
+        return ResponseEntity.ok(noteResponse);
+    }
+
+    @GetMapping("/notes")
+    public ResponseEntity<List<NoteResponse>> getAllTasks(){
+        List<NoteResponse> noteResponseList = noteService.getAllNotes();
+        return ResponseEntity.ok(noteResponseList);
+    }
+
+    @PutMapping("/notes/{id}")
+    public ResponseEntity<NoteResponse> updateNote(@PathVariable String id, @Valid @RequestBody NoteRequest noteRequest){
+        NoteResponse noteResponse = noteService.updateNoteById(id,noteRequest);
+        return ResponseEntity.ok(noteResponse);
+    }
+
+    @DeleteMapping("notes/{id}")
+    public ResponseEntity<NoteResponse> deleteNoteById(@PathVariable String id){
+        NoteResponse noteResponse = noteService.deleteNoteById(id);
+        return ResponseEntity.ok(noteResponse);
+    }
+}
